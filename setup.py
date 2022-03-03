e517e6dec70c988893ae2c95c94f7e72b4c3152d
GuillaumeShrp <guillaume.sherpa@gmail.com>
2022-03-03 16:23:15 +0100

    Import fix

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import find_packages
 
 
 setup(name='recurrentshop',
-      version='1.0.0',
+      version='1.1.0',
       description='Framework for building complex recurrent neural networks with Keras',
       author='Fariz Rahman',
       author_email='fariz@datalog.ai',
-      url='https://github.com/farizrahman4u/recurrentshop',
-      download_url='https://github.com/farizrahman4u/recurrentshop',
+      url='https://github.com/GuillaumeShrp/recurrentshop',
+      download_url='https://github.com/GuillaumeShrp/recurrentshop',
       license='MIT',
       install_requires=['keras'],
       packages=find_packages())
